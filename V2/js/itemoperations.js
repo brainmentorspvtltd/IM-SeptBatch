4f1346c48d9a13b68763d0703c185cd51cdbc9d3
amitsrivastava4all <amitsrivastava4all@gmail.com>
2018-09-19 14:01:08 +0530

    Code

---
+++ V2/js/itemoperations.js
@@ -0,0 +1,28 @@
+const itemOperations = {
+    items:[],
+    add(id , name, desc, color, date,price,url){
+        var item = new Item(id, name,desc, color,date,price,url);
+        this.items.push(item);
+        return item;
+    },
+    getTotalRecords(){
+        return this.items.length;
+    },
+    toggleMark(id){
+        var itemObject = this.searchById(id);
+        if(itemObject){
+            itemObject.toggle();
+        }
+    },
+    deleteMarked(){
+        this.items = this.items.filter(itemObject=>itemObject.isMarked==false);
+        return this.items;
+    },
+    searchById(id){
+        return this.items.find(itemObject=>itemObject.id==id);
+    },
+    countMarked(){
+        return this.items.filter(itemObject=>itemObject.isMarked).length;
+    }
+
+}
